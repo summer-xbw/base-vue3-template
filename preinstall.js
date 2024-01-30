99df12d3785c88b99608bbff8f765fc0bfda741c
summer-xbw <summer.xbw@foxmail.com>
2024-01-30 17:37:26 +0800

    限制包管理器

--- preinstall.js
+++
@@ -1,7 +0,0 @@
-console.log("执行了");
-if (!/pnpm/.test(process.env.npm_execpath || "")) {
-  console.warn(
-    `\u001b[33m This repository requires using pnpm as the package manager for scripts to work properly.\u001b[39m\n`
-  );
-  process.exit(1);
-}
